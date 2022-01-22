66b6f2ca290308a69f87b270fd7ab69f9edabbca
AlokSharma01 <aloksj788@gmail.com>
2022-01-23 01:24:11 +0530

    about page final push

--- javaScript/about.js
+++ javaScript/about.js
@@ -20,3 +20,24 @@ setInterval(function () {
  }
  document.getElementsByClassName("demo_class")[0].src = img[i];
 }, 5000);
+
+// nav bar
+
+var ham = document.getElementById("hamIcon");
+ham.addEventListener("click" , add_class_nav_right);
+
+function add_class_nav_right(){
+    var ham = document.getElementById("hamIcon");
+    var nav_right = document.querySelector(".nav-right-part");
+    nav_right.classList.toggle("nav-append");
+    ham.style.display = "none";
+    // console.log(nav_right)
+}
+var cross = document.getElementById("cross");
+cross.addEventListener("click", cross_nav);
+
+function cross_nav(){
+    var remove_nav = document.querySelector(".nav-append");
+    remove_nav.classList.remove("nav-append");
+    ham.style.display = "block";
+}
